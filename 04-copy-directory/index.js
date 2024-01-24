b51aa18870edb72fbffe0e6c9e768f39a36f1e33
albert <xsoularisx@gmail.com>
2024-01-24 20:52:40 +0300

    fix: 04 - add instructions when deleting a file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,22 +5,29 @@ const path = require('path');
 const mainFiles = path.join('./04-copy-directory/files');
 const copyFiles = path.join('./04-copy-directory/files-copy');
 
-fs.mkdir(copyFiles, { recursive: true }, (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  };
+fs.stat(copyFiles, (err) => {
+  if (!err) {
+    fs.rm(copyFiles, { recursive: true }, (err) => {
+      if (err) throw err;
+      makeFolder();
+    });
+  } else {
+    makeFolder();
+  }
 });
 
-fs.readdir(mainFiles, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  };
+function makeFolder() {
+  fs.mkdir(copyFiles, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 
-  files.forEach(el => {
-    fs.copyFile(`${mainFiles}/${el}`, `${copyFiles}/${el}`, err => {
-      if (err) throw err;
+  fs.readdir(mainFiles, (err, files) => {
+    if (err) throw err;
+
+    files.forEach(el => {
+      fs.copyFile(`${mainFiles}/${el}`, `${copyFiles}/${el}`, err => {
+        if (err) throw err;
+      });
     });
   });
-});
+}
